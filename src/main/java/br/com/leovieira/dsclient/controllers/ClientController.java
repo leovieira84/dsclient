64f8846c4d53378d2414e96b32f16c62738fd531
leovieira84 <leonardo.vieira@gmail.com>
2024-11-17 12:46:14 -0300

    Busca cliente por id

--- src/main/java/br/com/leovieira/dsclient/controllers/ClientController.java
+++ src/main/java/br/com/leovieira/dsclient/controllers/ClientController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,10 @@ public class ClientController {
 	@GetMapping
 	public Page<ClientDTO> allClients(Pageable pageable){
 		return service.findAll(pageable);
+	}
+	
+	@GetMapping("/{id}")
+	public ClientDTO clientById(@PathVariable Long id) {
+		return service.findById(id);
 	}
 }
