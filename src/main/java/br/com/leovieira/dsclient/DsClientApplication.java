1b41089b6dcc4a8d9256ced0123b35d89ff7531a
leovieira84 <leonardo.vieira@gmail.com>
2024-11-17 12:01:44 -0300

    Configuração inicial do projeto - Spring Initializr

---
+++ src/main/java/br/com/leovieira/dsclient/DsClientApplication.java
@@ -0,0 +1,13 @@
+package br.com.leovieira.dsclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/leovieira/dsclient/DsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leovieira.dsclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
