acccc2f8024d8eb5df6d174c6686138212aa48e5
leovieira84 <leonardo.vieira@gmail.com>
2024-11-17 12:51:29 -0300

    PUT Client

--- src/main/java/br/com/leovieira/dsclient/controllers/ClientController.java
+++ src/main/java/br/com/leovieira/dsclient/controllers/ClientController.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,5 +33,10 @@ public class ClientController {
 	@PostMapping
 	public ClientDTO saveClient(@RequestBody ClientDTO dto) {
 		return service.insert(dto);
+	}
+	
+	@PutMapping("/{id}")
+	public ClientDTO updateClient(@PathVariable Long id, @RequestBody ClientDTO dto) {
+		return service.update(id, dto);
 	}
 }
